ac4157c42e1b5ceb54ee22b25b7224041bf32b57
Aamir-K11 <amirkhan11999@gmail.com>
2023-04-01 03:49:21 +0500

    feat: added omit.ts

---
+++ omit.ts
@@ -0,0 +1,12 @@
+type OmitKeys<T, K extends keyof T> = { [P in keyof T as P extends K ? never: P]: T[P]  };
+
+
+
+type Person = {
+    id: string,
+    firstName: string,
+    lastName: string
+}
+
+type ID = OmitKeys<Person, 'firstName' | 'lastName'>
+
